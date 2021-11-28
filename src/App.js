2efb1ba2b41a79720012bcfe9d9fb79c490b9061
walle <dlauddn95@gmail.com>
2021-11-28 17:37:30 +0900

    :>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import './App.scss';
 import Button from './components/Button';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div className="buttons">
-        <Button>BUTTON</Button>
-      </div>
+        <div className="buttons">
+            <Button size='large'>BUTTON</Button>
+            <Button size='medium'>BUTTON</Button>
+            <Button size='small'>BUTTON</Button>
+        </div>
+
+        <div className="buttons">
+            <Button color='gray' size='large'>BUTTON</Button>
+            <Button color='gray' size='medium'>BUTTON</Button>
+            <Button color='gray' size='small'>BUTTON</Button>
+        </div>
+
+        <div className="buttons">
+            <Button color="pink" size='large'>BUTTON</Button>
+            <Button color="pink" size='medium'>BUTTON</Button>
+            <Button color="pink" size='small'>BUTTON</Button>
+        </div>
+
+        <div className="buttons">
+            <Button color="blue" size='large' outline>BUTTON</Button>
+            <Button color="gray" size='medium' outline>BUTTON</Button>
+            <Button color="pink" size='small' outline>BUTTON</Button>
+        </div>
+
+        <div className="buttons">
+            <Button size='large' fullWidth className="customized-button">BLACK+BUTTON</Button>
+            <Button color="gray" size='large' fullWidth>BUTTON</Button>
+            <Button 
+                color="pink"
+                size='large'
+                fullWidth 
+                onClick={() =>{
+                    console.log("클릭!")
+                }}
+                onMouseMove={() =>{
+                    console.log("마우스무비")
+                }}
+
+            > 
+            BUTTON
+            </Button>
+        </div>
     </div>
   );
 }
